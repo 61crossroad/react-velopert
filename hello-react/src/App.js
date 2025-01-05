3090bece22cdba7b489fad2ed6d85fbdcfbf312f
Kim Seunghwan <61crossroad@gmail.com>
2025-01-06 03:50:33 +0900

    jsx grammars and styling

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// import { Fragment } from 'react';
 
+/*
 function App() {
   return (
     <div className="App">
@@ -20,6 +22,39 @@ function App() {
       </header>
     </div>
   );
+}
+*/
+function App() {
+  const name = "React";
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'blod',
+    padding: 16
+  };
+
+  return (
+    // style doesn't work with Fragment
+    // <div style={style}>
+    // first { is a javascript expression, then second { for javascript object
+    <div 
+      style={{
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'blod',
+        padding: 16
+      }}>
+      {name === 'React' ? (
+        <h1>It's a {name}</h1>
+      ) : (
+        <h1> It's not a React</h1>
+      )}
+      {/* <h1>Hello! {name}</h1> */}
+      <h2>How are you?</h2>
+    </div>
+  )
 }
 
 export default App;
