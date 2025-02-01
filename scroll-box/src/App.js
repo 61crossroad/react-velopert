418154d8029d7be45a31c530faf00627f8d733d6
Kim Seunghwan <61crossroad@gmail.com>
2025-02-02 04:10:30 +0900

    ref for scroll bar control

---
+++ scroll-box/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import ScrollBox from "./ScrollBox";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref} />
+          <button onClick={() => this.scrollBox.scrollToBottom()}>To bottom</button>
+      </div>
+    );
+  }
+}
+
+export default App;
