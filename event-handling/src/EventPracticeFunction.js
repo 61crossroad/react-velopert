4bce386ba5eb60c8bd20995cd46225bfdef4c3b8
Kim Seunghwan <61crossroad@gmail.com>
2025-02-02 03:15:33 +0900

    object in useState

--- event-handling/src/EventPracticeFunction.js
+++ event-handling/src/EventPracticeFunction.js
@@ -1,20 +1,42 @@
 import { useState } from "react";
 
 const EventPractice = () => {
+    /*
     const [username, setUsername] = useState('');
     const [message, setMessage] = useState('');
     const onChangeUsername = e => setUsername(e.target.value);
     const onChangeMessage = e => setMessage(e.target.value);
+    */
+
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
+    const {username, message} = form;
+    const onChange = e => {
+        const nextForm = {
+            ...form,
+            [e.target.name]: e.target.value
+        };
+        setForm(nextForm);
+    };
     const onClick = () => {
         alert(username + ': ' + message);
+        /*
         setUsername('');
         setMessage('');
+        */
+       setForm({
+        username: '',
+        message: ''
+       })
     };
     const onKeyDown = e => {
         if (e.key === 'Enter') {
             onClick();
         }
     };
+
     return (
         <div>
             <h1>Event Practice (function)</h1>
@@ -23,14 +45,16 @@ const EventPractice = () => {
                 name="username"
                 placeholder="Username"
                 value={username}
-                onChange={onChangeUsername}
+                // onChange={onChangeUsername}
+                onChange={onChange}
             />
             <input
                 type="text"
                 name="message"
                 placeholder="Type anything"
                 value={message}
-                onChange={onChangeMessage}
+                // onChange={onChangeMessage}
+                onChange={onChange}
                 onKeyDown={onKeyDown}
             />
             <button onClick={onClick}>Confirm</button>
